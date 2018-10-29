00951072a6760430dd31660e6f6e7ff08ca2f0b4
jaimeee5 <36453060+jaimeee5@users.noreply.github.com>
2018-10-29 11:30:26 +0100

    Add files via upload

---
+++ HelloWorld/src/HeloWorld.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : HeloWorld.cpp
+// Author      : Jaime
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a=3;
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << a;
+	return 0;
+}
