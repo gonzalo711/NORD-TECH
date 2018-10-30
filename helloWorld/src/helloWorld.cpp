d2b5cadb031cb73f02c35a45cb87faf4b5ce22b6
rodrigoladero <rodrigo.lg95@gmail.com>
2018-10-30 09:32:31 +0100

    helloWorld project added

---
+++ helloWorld/src/helloWorld.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : HeloWorld.cpp
+// Author      : Rodrigo Ladero
+// Version     : 1.0
+// Copyright   :
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout<<"Hello World"<<endl;
+
+	return 0;
+}
