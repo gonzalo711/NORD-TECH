3cc252dc7e6b45bf7fc382732366ef34287f1375
Daniel <Daniel>
2018-11-03 13:21:35 +0100

    helloWorld Dani

---
+++ helloWorld/src/helloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : helloWorld.cpp
+// Author      : Daniel
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World de Dani" << endl; // prints !!!Hello World!!!
+	return 0;
+}
