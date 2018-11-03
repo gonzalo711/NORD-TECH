f75f94c103838c158a171adee3a68878ce5f6d56
Jaime <Jaime@HPJaime.home>
2018-11-03 19:42:39 +0100

    .

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Jaime
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!! It's Jaime" << endl; // prints Hello World!!!
+	return 0;
+}
