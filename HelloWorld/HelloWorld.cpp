2383d01174450fa68a7a01f17653c141749e9353
Propietario <Propietario@ASUS>
2018-11-04 17:49:07 +0100

    -

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Elena Crespo
+// Version     :
+// Copyright   :
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello world!!!" << endl; // prints Hello world!!
+	return 0;
+}
+
