e76d94523aceced027ecbb068ed967da00d05927
montse-dcd <montse.dcd@gmail.com>
2018-11-04 21:19:05 +0100

    Hello World MDCD

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,11 @@
+// Name        : HelloWorld.cpp
+// Author      : Montse Diaz-Carrasco Diaz
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello wold!!" << endl; // prints Hello wold!!
+	return 0;
+}
